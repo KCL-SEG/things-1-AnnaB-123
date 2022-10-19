8ea9436034c6b6fda5b15b1d5b237bb01ce43447
Anna Brown <anna.2.brown@kcl.ac.uk>
2022-10-19 22:58:58 +0100

    adds model to settings

--- things/models.py
+++ things/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator 
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db.models import Model
 
 # Create your models here.
-class Thing(models.Model):
+class Thing(Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.CharField(max_length=120, unique=False, blank=True)
     quantity = models.IntegerField(unique=False, blank=False,validators=[MinValueValidator(0), MaxValueValidator(100)])
