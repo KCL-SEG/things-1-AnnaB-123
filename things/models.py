92c576a2e8e27cb9893230840dc8ca44203e2217
Anna Brown <anna.2.brown@kcl.ac.uk>
2022-10-19 22:52:06 +0100

    corrections to model

--- things/models.py
+++ things/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.db.models.Model import Model
-
+from django.core.validators import MaxValueValidator, MinValueValidator 
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.charField(max_length=30, unique=True, blank=False)
-    description = models.charField(max_length=120, unique=False, blank=True)
-    quantity = models.IntegerField(unique=False, blank=False, max_length=100, min_length=0)
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.CharField(max_length=120, unique=False, blank=True)
+    quantity = models.IntegerField(unique=False, blank=False,validators=[MinValueValidator(0), MaxValueValidator(100)])
